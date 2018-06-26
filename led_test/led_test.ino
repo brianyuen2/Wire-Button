e87799a40af42c5946a300a9bb3b2dda3fe7d7fa
brian <brianyuen2@gmail.com>
2018-06-26 17:24:27 +1000

    cleaned up code

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -2,13 +2,11 @@ int broken = 0;
 int start = 0;
 unsigned long time = 0;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(9, OUTPUT);
   pinMode(3, INPUT);
 
 }
-
 void loop() {
   if (start == 0) {
     start = 1;
@@ -16,38 +14,18 @@ void loop() {
   }
   time = millis();
   //Serial.println(time);
-
   // time condition
   if(time % 5000 == 0) {
     broken = 0;
     digitalWrite(9, HIGH);
     Serial.println("5 seconds");
   }
-
-  //Serial.print("Pin3 Input: ");
-  //Serial.println(digitalRead(3));
-  
-  // read pin3 input
+  // If broken do this or that
   if(digitalRead(3) == 0) {
     broken = 1;
-    digitalWrite(9, LOW);
-    
+    digitalWrite(9, LOW);    
   } else {
     broken = 0;
     digitalWrite(9, HIGH);
-  }
-  
-  // not broken
-  /**if(broken == 0) {
-    //Serial.println(time);
-    digitalWrite(9, HIGH);
-      // 5 seconds stage
-  
-  // broken
-  } else {
-      digitalWrite(9, LOW);
-  }**/
-  //Serial.println(broken);
-  //delay(1000);
-
+  }  
 }
